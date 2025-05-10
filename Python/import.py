aea6974fe7e4942b3a0aa676b647d5b78eda0d09
60gCreatine <cphah643@gmail.com>
2025-05-10 22:17:58 +0200

    split up into .R and .py

---
+++ Python/import.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+# Import data
+matches_raw = pd.read_csv("data/atp_matches_till_2022.csv")
+players_raw = pd.read_csv("data/atp_players_till_2022.csv")
+rankings_raw = pd.read_csv("data/atp_rankings_till_2022.csv")
---
+++ R/import.R
@@ -0,0 +1,4 @@
+# Import
+matches_raw <- read.csv("data/atp_matches_till_2022.csv")
+players_raw <- read.csv("data/atp_players_till_2022.csv")
+rankings_raw <- read.csv("data/atp_rankings_till_2022.csv")
